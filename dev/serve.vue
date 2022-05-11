cd7d4dbc13d6304a6773dd870fe300532772d209
Victor Pereira <victor.smsrio@gmail.com>
2022-05-11 15:09:30 -0300

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import Vue3BasicAutocomplete from '@/vue3-basic-autocomplete.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    Vue3BasicAutocomplete
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue3-basic-autocomplete />
+  </div>
+</template>
